1a5388a8cefb27275868fa4a125406c55645bd48
Gustavo de Oliveira Fagundes <93541091+TavaoBR@users.noreply.github.com>
2024-12-04 16:11:53 -0300

    Ajuste no ProdutoController
    Alterado os endpoints da classe, agora apenas serão feitas requisições do metodo GET, para listar os produtos e detalhe de um produto especifico no front-end

---
+++ BackEnd/src/Controller/ProdutoController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProdutoRepository;
+
+class ProdutoController extends AbstractController
+{
+    #[Route('/produtos', name: 'produtos_lista', methods: ['GET'])]
+    public function index(ProdutoRepository $produtoRepository, Request $request): JsonResponse
+    {
+
+        $validacao = $this->validarRequest($request);
+        if ($validacao !== null) return $validacao;
+
+
+        $dados = $produtoRepository->findAll();
+
+        if(count($dados) == 0) return $this->json([
+           'status' => 404,
+           'message' => 'Nenhum Produto Encontrado'
+        ]);
+
+
+        return $this->json([
+            'status' => 200,
+            'data' =>  $dados
+        ]);
+    }
+
+
+    #[Route('/produtos/{id}', name: 'produtos_id', methods: ['GET'])]
+    public function produto(int $id,Request $request, ProdutoRepository $produtoRepository): JsonResponse
+    {
+
+        $validacao = $this->validarRequest($request, $id);
+        if ($validacao !== null) return $validacao;
+
+        $produto = $produtoRepository->find($id);
+
+        if(!$produto) return $this->json([
+           'status' => 404,
+           'message' => 'Produto Encontrado'
+        ]);
+
+
+        return $this->json([
+            'status' => 200,
+            'message' => 'Produto encontrado',
+            'data' => $produto
+        ]);
+    }
+
+
+    private function validarRequest(Request $request, ?int $id = null): ?JsonResponse
+    {
+        if (count($request->query->all()) > 0) return $this->json([
+                'status' => 400,
+                'message' => 'Parâmetros adicionais na query string não são permitidos.',
+            ]);
+        
+
+        if (!empty($request->getContent())) return $this->json([
+                'status' => 400,
+                'message' => 'Dados no corpo da requisição não são permitidos.',
+            ]);
+        
+
+        if ($id !== null && $id <= 0) return $this->json([
+                'status' => 400,
+                'message' => 'O ID fornecido não é válido.',
+            ]);
+        
+
+        return null;
+    }
+
+
+}
