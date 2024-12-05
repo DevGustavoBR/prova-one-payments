444198f6673decf58fa484893dfda71351a6166f
Gustavo de Oliveira Fagundes <93541091+TavaoBR@users.noreply.github.com>
2024-12-04 23:50:35 -0300

    Componente de Erro
    Criado um component para exibir mensagem de erro, será usado em varias paginas, principal nas que fazem requisição do tipo GET para API

---
+++ frontEnd/src/components/ErrorComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+const props = defineProps({
+  error: {type: String,required: true}
+});
+
+
+</script>
+
+
+<template>
+<div v-if="props.error" class="bg-gray-50 text-gray-800 p-8 w-full rounded-lg font-[sans-serif] max-w-4xl mx-auto py-36">
+      <h1 class="text-4xl font-extrabold  text-center">{{ props.error }}</h1>
+    </div>
+</template>
