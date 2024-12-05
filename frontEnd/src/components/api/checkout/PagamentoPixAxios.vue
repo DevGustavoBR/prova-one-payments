0ed3ee04a5fcf990546dbba6adf0279ac852d668
Gustavo de Oliveira Fagundes <93541091+TavaoBR@users.noreply.github.com>
2024-12-05 00:23:18 -0300

    Criado View e Component Pix
    Em PagamentoPixAxios, é onde será exibido as informações da transação que são pix, nela teremos informações como Qrcode e chave do pix, além de contar com button para processar pagamento, ao clicar será feita requisição PUT para API onde iremos atualizar os status da transação para aprovada ou negada, como é uma simulação, a cada requisição será atribuido um status diferente, após isso caso não dê erro na API, será direcionado para tela de detalhes da transação onde mostrará se foi aprovada ou negada

---
+++ frontEnd/src/components/api/checkout/PagamentoPixAxios.vue
@@ -0,0 +1,134 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+import BotaoProcessarPagamentoAxios from '../BotaoProcessarPagamentoAxios.vue';
+import ErrorComponent from '@/components/ErrorComponent.vue';
+
+const props = defineProps({
+  transacaoId: {
+    type: String,
+    required: true
+  }
+});
+
+const detalhes = ref({});
+const loading = ref(true);
+const error = ref(null);
+const router = useRouter();
+
+const transacaoId = props.transacaoId;
+const url = `http://127.0.0.1:8000/api/transacao/processar/pagamento/pix/${props.transacaoId}`;
+
+const apiTransacao = async () => {
+  try{
+
+    const response = await axios.get(`http://127.0.0.1:8000/api/transacao/${transacaoId}`);
+
+    if(response.data.status === 404){
+
+      error.value = response.data.message;
+      return;
+
+    }else{
+
+      const data = response.data;
+
+      if(data.dados.detalhes.metodo_pagamento !== "PIX"){
+        error.value = "Essa transação não pertence ao metodo pix"
+        router.push(`/checkout/pagamento/cartao-credito/${transacaoId}`);
+        return;
+      }
+
+      if(data.dados.status !== 'pedente'){
+         error.value = 'Essa transação já foi processada';
+         return;
+      }
+
+      detalhes.value = {
+        status: data.dados.status,
+        cliente: data.dados.detalhes.cliente,
+        produto: data.dados.detalhes.produto,
+        valor: data.dados.detalhes.valor,
+        metodo_pagamento: data.dados.detalhes.metodo_pagamento,
+        pix_payload: data.dados.detalhes.pix_payload,
+        qr_code: data.dados.detalhes.qr_code
+      }
+
+
+
+    }
+
+  }catch(err){
+
+    error.value = "Erro ao buscar os dados da transação.";
+    console.error(err);
+
+  }finally{
+
+    loading.value = false;
+  }
+
+}
+
+onMounted(apiTransacao);
+
+</script>
+
+<template>
+
+<ErrorComponent :error="error"/>
+
+
+  <div v-if="!error" class="font-[sans-serif] bg-white ">
+      <div class="max-lg:max-w-xl mx-auto w-full">
+        <div class="grid lg:grid-cols-3 gap-6">
+          <div class="lg:col-span-2 max-lg:order-1 p-6 !pr-0 max-w-4xl mx-auto w-full">
+            <div class="text-center max-lg:hidden">
+              <h2 class="text-3xl font-extrabold text-gray-800 inline-block border-b-2 border-gray-800 pb-1">Checkout</h2>
+            </div>
+
+
+            <div class="text-center max-lg:hidden py-5">
+              <p>Atenção, é apenas uma simulação</p>
+              <p>Essa chave e Qrcode não funcionam, está apenas para simular o metodo de pagamento 'PIX'</p>
+              <span></span>
+            </div>
+
+              <div class="py-2">
+                <h2 class="text-xl font-bold text-gray-800">Qrcode Pix</h2>
+               <img :src="detalhes.qr_code" alt="QR Code Pix" />
+              </div>
+
+              <div class="mt-16">
+                <h2 class="text-xl font-bold text-gray-800">Chave Pix</h2>
+                 <p class="font-bold text-gray-800">{{ detalhes.pix_payload }}</p>
+              </div>
+
+              <div class="flex flex-wrap gap-4 mt-8">
+                <BotaoProcessarPagamentoAxios
+                type="button"
+                class="min-w-[150px] px-6 py-3.5 text-sm bg-blue-600 text-white rounded-lg hover:bg-blue-700"
+                :url="url"
+                label="Processar Pagamento"
+                />
+              </div>
+          </div>
+
+          <div class="bg-gray-100 p-6 rounded-md">
+            <h2 class="text-4xl font-extrabold text-gray-800">R$ {{ detalhes.valor  }}</h2>
+
+            <ul class="text-gray-800 mt-8 space-y-4">
+              <li class="flex flex-wrap gap-4 text-sm">{{ detalhes.produto }} <span class="ml-auto font-bold">R$ {{ detalhes.valor  }}</span></li>
+              <li class="flex flex-wrap gap-4 text-sm">Descontos <span class="ml-auto font-bold">R$ 00.00</span></li>
+              <li class="flex flex-wrap gap-4 text-sm">Taxas <span class="ml-auto font-bold">R$ 00.00</span></li>
+              <li class="flex flex-wrap gap-4 text-sm font-bold border-t-2 pt-4">Total <span class="ml-auto">R$ {{ detalhes.valor  }}</span></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+
+
---
+++ frontEnd/src/views/checkout/pix/PixPage.vue
@@ -0,0 +1,12 @@
+<script setup>
+ import { useRoute } from 'vue-router';
+ import PagamentoPixAxios from '@/components/api/checkout/PagamentoPixAxios.vue';
+
+ const route = useRoute();
+ const transacaoId = route.params.id;
+</script>
+
+<template>
+ <PagamentoPixAxios :transacaoId="transacaoId"/>
+</template>
+
