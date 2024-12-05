871e84dafa0c81402f7e89375cb2cab93b4e7443
Gustavo de Oliveira Fagundes <93541091+TavaoBR@users.noreply.github.com>
2024-12-04 23:48:40 -0300

    Arquivo principal
    Arquivo onde será renderizado as views

---
+++ frontEnd/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {RouterView} from 'vue-router'
+import NavBar from './components/menu/NavBar.vue'
+
+</script>
+
+<template>
+
+  <NavBar />
+
+  <main >
+    <RouterView />
+  </main>
+</template>
+
+
